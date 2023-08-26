7663bac680b85145741992985fe42dc323108332
Rodrigo Lessa <rodrigo.lessa.dev@gmail.com>
2023-08-25 21:06:05 -0300

    subindo para mesclar com do Mateus

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,12 @@
-var instance = M.Sidenav.getInstance(elem);
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
 });
 
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-// var collapsibleElem = document.querySelector('.collapsible');
-// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
 // Or with jQuery
 
-$(document).ready(function () {
+$(document).ready(function(){
   $('.sidenav').sidenav();
-}); 
+});
+      
