da4ef52e12a2ab6f8fecfa0039332728dcadf328
Rodrigo Lessa <rodrigo.lessa.dev@gmail.com>
2023-08-28 19:44:09 -0300

    Finalização

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,30 @@
 // Responsável pelo menu mobile
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
 });
 
 // Or with jQuery
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('.sidenav').sidenav();
 });
 
+// var instance = M.Tabs.init(el, options);
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $('.tabs').tabs();
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.scrollspy');
+//   var instances = M.ScrollSpy.init(elems, options);
+// });
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $('.scrollspy').scrollSpy();
+});
